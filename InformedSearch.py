86343ad09abadc9398656296b08bb641cadc9aab
Victor Arreaga <arreagav17@gmail>
2022-02-22 21:47:00 -0500

    refactor

--- InformedSearch.py
+++ InformedSearch.py
@@ -1,62 +1,93 @@
+import datetime
 from abc import ABC, abstractmethod
-from Node import Node
+from Vertex import Vertex
 from TypesInformedSearch import TypesInformedSearch
 
 
 class InformedSearch(ABC):
-    def __init__(self, start: Node, goal: Node) -> None:
-        # self.__types_informed_search = types_informed_search
+    def __init__(self, start: Vertex, goal: Vertex) -> None:
         self.__start = start
         self.__goal = goal
+        self.__times = {}
 
     def shortest_path(self):
-        visited_node = self.__start
-        visited_node.set_cost_for_childrens()
-        children_of_nodes_visited: list[Node] = visited_node.get_child_keys()
-        while self._search_condition(children_of_nodes_visited):
-            visited_node = children_of_nodes_visited[0]
-            children_of_nodes_visited.remove(visited_node)
-            visited_node.set_cost_for_childrens()
-            for node in visited_node.get_child_keys():
-                children_of_nodes_visited.append(node)
+        start_time = datetime.datetime.now()
+        iterations = 1
+        visited_vertex = self.__start
+        children_of_vertexs_visited: list[Vertex] = self.__sorted(
+            visited_vertex.add_costs_for_childrens()
+        )
+        self.__show(iterations, visited_vertex, children_of_vertexs_visited)
 
-            children_of_nodes_visited = sorted(
-                list(set(children_of_nodes_visited)),
-                key=lambda node: node,
-            )
+        while self._search_condition(children_of_vertexs_visited):
+            iterations += 1
+            visited_vertex = children_of_vertexs_visited[0]
+            children_of_vertexs_visited.remove(visited_vertex)
+            self.__show(iterations, visited_vertex, children_of_vertexs_visited)
+            for vertex in visited_vertex.add_costs_for_childrens(self.__start):
+                children_of_vertexs_visited.append(vertex)
+
+            children_of_vertexs_visited = self.__sorted((children_of_vertexs_visited))
+
+        end_time = datetime.datetime.now()
+
+        time_diff = end_time - start_time
+        execution_time = time_diff.total_seconds() * 1000
+        print(">>>Resultados<<<")
+        print(f"Número de iteraciones: {iterations}")
+        print(f"Tiempo de ejecución: {execution_time:.3f}ms")
+
+    def __sorted(self, vertexs: list[Vertex]) -> list[Vertex]:
+        return sorted(vertexs, key=lambda vertex: vertex)
+
+    def __show(
+        self,
+        iterations: int,
+        visited_vertex: Vertex,
+        vertexs: list[Vertex],
+    ) -> None:
+        print(f"ANALIZAR: {visited_vertex} ITERACION: {iterations} ")
+        for i in vertexs:
+            print(f"EN COLA {i} -> {i.get_parent()}: {i.get_cost()}")
+        print("###################")
 
     @abstractmethod
-    def _search_condition(self, children_of_nodes_visited: list[Node]) -> bool:
+    def _search_condition(self, children_of_vertexs_visited: list[Vertex]) -> bool:
         pass
 
     def _show_path(self, only_last_cost: bool = False) -> None:
-        if self.__goal.have_parent():
+        if self.exists_route():
             cost = 0
-            nodes = self.get_route()
-            start, goal = nodes[0], nodes[-1]
+            vertexs = self.get_route()
+            start, goal = vertexs[0], vertexs[-1]
+
             cost = goal.get_cost()
 
             print(f"Ruta encontrada!!!")
             print(f"\t> Primero comienza en {start}")
 
-            for node in nodes[1:-1]:
+            for vertex in vertexs[1:-1]:
                 print(
-                    f"\t\t>Luego continua hacia {node} que tiene un costo de {round(node.get_cost(), 2)}"
+                    f"\t\t>Luego continua hacia {vertex} que tiene un costo de {vertex.get_cost()}"
                 )
-                if not only_last_cost:
-                    cost += node.get_cost()
-            print(
-                f"\t> Y finaliza en {goal} tiene un costo de {round(goal.get_cost(), 2)}"
-            )
-            cost += goal.get_cost()
-            print(f"Total costo: {round(cost, 2)}")
+                cost += vertex.get_cost()
+            print(f"\t> Y finaliza en {goal} que tiene un costo de {goal.get_cost()}")
+            if only_last_cost:
+                cost = goal.get_cost()
+            print(f"Total costo: {cost}")
         else:
             print(
                 f"No se ha encontrado una ruta para ir de {self.__start} a {self.__goal}"
             )
 
-    def get_goal(self) -> Node:
+    def get_goal(self) -> Vertex:
         return self.__goal
 
-    def get_route(self) -> list[Node]:
+    def get_route(self) -> list[Vertex]:
         return list(self.__goal.get_parents().__reversed__())
+
+    def exists_route(self) -> bool:
+        return self.__goal.have_parent()
+
+    def _get_start(self) -> Vertex:
+        return self.__start
